5a2b932005ff8d4ab95c0b9d608dc8a23b84bfc3
Ati1707 <152104750+Ati1707@users.noreply.github.com>
2024-08-24 17:06:44 +0200

    add functions

---
+++ src/__init__.py
@@ -0,0 +1,11 @@
+from src.downloader import (
+    is_ffmpeg_installed,
+    delete_existing_ffmpeg,
+    extract_ffmpeg,
+    download_ffmpeg,
+    get_latest_ffmpeg_version,
+    get_local_ffmpeg_version,
+    is_ffmpeg_update_available,
+    move_ffmpeg_binaries,
+    check_download_ffmpeg
+)
